1301214d402e74c9cf043f7713647446e681ba3b
Bateyjosue <josuebatey19@gmail.com>
2022-09-13 07:16:00 +0200

    render the InputTodo component to the TodoComponent

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import TodoList from './TodoList';
 import Header from './Header';
+import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends Component {
   constructor(props) {
@@ -26,12 +28,49 @@ class TodoContainer extends Component {
     };
   }
 
+  handleChange = (id) => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map((todo) => {
+        if(todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo;
+      })
+    }));
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    })
+  }
+
+  addTodoItem = (item) => {
+    const newTodo = {
+      id: uuidv4(),
+      title:item,
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    })
+
+  }
+
   render() {
     return (
       <div>
         <Header />
         {/* eslint-disable-next-line react/destructuring-assignment */}
-        <TodoList todos={this.state.todos} />
+        <InputTodo addTodoProps={this.addTodoItem} />
+        <TodoList todos={this.state.todos} handleChangeProps={this.handleChange} handleDelete={this.delTodo} />
       </div>
     );
   }
